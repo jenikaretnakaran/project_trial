6238f99ad5914d436c66de5dadd81d7409f5273f
jenikaretnakaran <jenikav952@gmail.com>
2023-01-03 19:02:54 +0530

    testcases update

--- contracts/Bonus.sol
+++ contracts/Bonus.sol
@@ -33,12 +33,99 @@ modifier onlySuperAdmin() {
         _;
 }
 
-// adding fund to the contract
+// function to add fund to the contract
 function addFund(uint256 amount) public payable onlySuperAdmin {
     require(amount > 0 ,"no amount is specified");
     _mint(address(this),amount);
 //     transfer(address(this),amount);
 }
+
+// function to add employee details
+function addEmployee(uint256 _id,
+                uint256 _salary,
+                address _address,
+                bool _isRestricted) public onlySuperAdmin 
+{       
+        require(employeeList[_address].id != _id && employeeList[_address].salaryAddress != _address,"Employee with this /id already exists");
+        employee memory newEmployee = employee(_id, _salary, _address, block.timestamp, _isRestricted);
+        employeeList[_address] = newEmployee;
+        claimList[_address].push(block.timestamp);
+}
+//function to upgrade employee status
+function upgradeStatus(uint256 _salary,
+                address _key,
+                bool isRestricted) public onlySuperAdmin
+{
+        employeeList[_key].salary = _salary;
+        employeeList[_key].isRestricted = isRestricted;
+}
+//function returns timestamp of the most recent time that the employee claimed their salary
+function salaryTimeStamp(address _key) public view returns(uint256) {
+        require(claimList[_key].length > 0, "Array is empty");
+        uint256 lastIndex = claimList[_key].length - 1;
+        uint256 lastItem = claimList[_key][lastIndex];
+        return lastItem;
+}
+//function returns timestamp of the most recent time that the employee claimed their incentives
+function incentiveTimeStamp(address _key) public view returns(uint256) {
+        uint256 length = claimList[_key].length;
+        require(claimList[_key][length - 1]-claimList[_key][length - 3] >= 15 seconds ,"15 seconds not reached");
+        uint256 incentive = employeeList[_key].salary;
+        uint256 result = incentive.mul(10).div(100);
+        return result;
+
+}
+
+//view employeedata
+function viewData(address _key) public view returns(uint256){
+        return employeeList[_key].salary;
+}
+
+//function inherited from ERC20 contract to transfer tokens
+function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        address owner = address(this);
+        _transfer(owner, to, amount);
+        return true;
+}
+//function for releasing salaryfund to employee
+function salaryClaim(address payable _sender) public {
+        require(employeeList[_sender].isRestricted == false ,"Unauthorized entry of employer");
+        uint256 blockTime = salaryTimeStamp(_sender);
+        require(block.timestamp - blockTime >= 5 seconds,"5 seconds not reached");
+        uint256 salary = employeeList[_sender].salary;
+        transfer(_sender,salary);
+        claimList[_sender].push(block.timestamp);
+}
+//function for releasing incentivefund to employee
+function incentiveClaim(address payable _sender) public {
+        require(employeeList[_sender].isRestricted == false ,"Unauthorized entry of employer");
+        uint256 _result = incentiveTimeStamp(_sender);
+        transfer(_sender,_result);
+        incentiveList[_sender].push(block.timestamp);
+
+}
+//function to get the totalsupply and balanceof tokens
+function balanceAndSupply() public view returns(uint256,uint256){
+        uint256 totalSupply = totalSupply();
+        uint256 balanceOf = balanceOf(address(this));
+        return (totalSupply,balanceOf); 
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // //adding admins
 // function addAdmin(uint256 _id, address _adminAdd) public onlySuperAdmin{
@@ -56,74 +143,3 @@ function addFund(uint256 amount) public payable onlySuperAdmin {
 //         require(Address.exists(adminList,msg.sender), "Account not exists");
 //         _;
 // }
-
-//adding employees
-function addEmployee(uint256 _id,
-                uint256 _salary,
-                address _address,
-                bool _isRestricted) public onlySuperAdmin 
-{
-        employee memory newEmployee = employee(_id, _salary, _address, block.timestamp, _isRestricted);
-        employeeList[_address] = newEmployee;
-        claimList[_address].push(block.timestamp);
-}
-//upgrade employee status
-function upgradeStatus(uint256 _salary,
-                address _key,
-                bool isRestricted) public onlySuperAdmin
-{
-        employeeList[_key].salary = _salary;
-        employeeList[_key].isRestricted = isRestricted;
-}
-//function to get last timeblock of salaryclaim
-function salaryTimeStamp(address _key) public view returns(uint256) {
-        require(claimList[_key].length > 0, "Array is empty");
-        uint256 lastIndex = claimList[_key].length - 1;
-        uint256 lastItem = claimList[_key][lastIndex];
-        return lastItem;
-}
-//function to get last timeblock of incentiveclaim
-function incentiveTimeStamp(address _key) public view returns(uint256) {
-        uint256 length = claimList[_key].length;
-        require(claimList[_key][length - 1]-claimList[_key][length - 3] >= 15 seconds ,"15 seconds not reached");
-        uint256 incentive = employeeList[_key].salary;
-        uint256 result = incentive.mul(10).div(100);
-        return result;
-
-}
-
-//view employeedata
-function viewData(address _key) public view returns(uint256){
-        return employeeList[_key].salary;
-}
-
-//function from ERC20 contract to transfer tokens
-function transfer(address to, uint256 amount) public virtual override returns (bool) {
-        address owner = address(this);
-        _transfer(owner, to, amount);
-        return true;
-}
-//releasing fund to employees
-function salaryClaim(address payable _sender) public {
-        require(employeeList[_sender].isRestricted == false ,"Unauthorized entry of employer");
-        uint256 blockTime = salaryTimeStamp(_sender);
-        require(block.timestamp - blockTime >= 5 seconds,"5 seconds not reached");
-        uint256 salary = employeeList[_sender].salary;
-        transfer(_sender,salary);
-        claimList[_sender].push(block.timestamp);
-}
-
-function incentiveClaim(address payable _sender) public {
-        require(employeeList[_sender].isRestricted == false ,"Unauthorized entry of employer");
-        uint256 _result = incentiveTimeStamp(_sender);
-        transfer(_sender,_result);
-        incentiveList[_sender].push(block.timestamp);
-
-}
-//to get the totalsupply and balanceof
-function balanceAndSupply() public view returns(uint256,uint256){
-        uint256 totalSupply = totalSupply();
-        uint256 balanceOf = balanceOf(address(this));
-        return (totalSupply,balanceOf); 
-}
-}
--- scripts/run.js
+++ scripts/run.js
@@ -3,13 +3,14 @@ const main = async () => {
     const bonusContractFactory = await hre.ethers.getContractFactory("payroll");
     const bonusContract = await bonusContractFactory.deploy();
     await bonusContract.deployed();
+    
     console.log("contract address:" , bonusContract.address);
     console.log("owner address:" , owner.address);
 
     const Web3 = require('web3');
     const web3 = new Web3();
 
-    const userInput = 5000000000; // Replace with the value entered by the user
+    const userInput = 1000000000;
     const value = web3.utils.toHex(userInput);
 
     const _addFund = await bonusContract.addFund(value);
@@ -19,15 +20,35 @@ const main = async () => {
     const _balanceAndSupply = await bonusContract.balanceAndSupply();
     console.log(_balanceAndSupply);
 
-    const _addEmployee = await bonusContract.addEmployee("1234","5000","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","false");
-    await _addEmployee.wait();
-    console.log(_addEmployee);
+    const _addEmployee1 = await bonusContract.addEmployee("1234","5000","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",false);
+    await _addEmployee1.wait();
+    const _addedEmployee1 = await bonusContract.employeeList("0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2");
+    console.log(_addedEmployee1);
 
-    const _upgradeStatus = await bonusContract.upgradeStatus("6000","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","false");
-    await _upgradeStatus.wait();
-    console.log("status updated");
+    // const _upgradeStatus = await bonusContract.upgradeStatus("6000","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",false);
+    // await _upgradeStatus.wait();
+    // const _upgraded = await bonusContract.employeeList("0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2");
+    // console.log(_upgraded);
 
-    
+    // const _addEmployee2 = await bonusContract.addEmployee("0001","4000","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",false);
+    // await _addEmployee2.wait();
+    // const _addedEmployee2 = await bonusContract.employeeList("0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db");
+    // console.log(_addedEmployee2);
+
+    const _salaryTimeStamp = await bonusContract.salaryTimeStamp("0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2");
+    console.log(_salaryTimeStamp);
+
+    t
+
+    // console.log("inside setTimeout");   
+    //   async () => {
+    //     const _salaryClaim = await bonusContract.salaryClaim("0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2");
+    //     await _salaryClaim.wait();
+    //     const result = await bonusContract.claimList("0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2");
+    //     console.log(result);
+    //   };
+    //    console.log("After setTimeout");
+
 };
 
 const runMain = async () => {
